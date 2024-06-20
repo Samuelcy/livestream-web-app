f1f414cb97548f0555240683e14322d4bbe0dd98
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-20 10:34:06 -1000

    Encrypt password

--- server/src/controllers/auth/postRegister.js
+++ server/src/controllers/auth/postRegister.js
@@ -1,12 +1,32 @@
 import User from "../../../models/User.js";
+import bcrypt from "bcryptjs"
 
-export const postRegister = async (req,res) => {
-    // Sends User to DB
-    const user = await User.create({
-        username: "test",
-        email: 'test@test.com',
-        password: 'test',
-    });
+export const postRegister = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const userExists = await User.exists({ email })
+        
+        if (userExists) {
+            return res.stats(409).send("Email already in use");
+        }
+
+        const encryptedPassword = await bcrypt.hash(password, 10);
+    
+        const user = await User.create({
+            username,
+            email: email.toLowerCase(),
+            password: encryptedPassword,
+        })
+
+        // Create JWT token
+        
+
+        // Send response back to user
+
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send('Error occured.')
+    }
 
     return res.send("User is added to database");
 }
