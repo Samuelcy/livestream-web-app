a8470b0d9b6e219a59db5b801ceabc6d8a65fe3d
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-20 13:52:57 -1000

    Add auth and login components
    isLogin button is switches between login and register when clicked

--- client/src/AuthPage/AuthPage.js
+++ client/src/AuthPage/AuthPage.js
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { Login } from "./Login";
+import { Register } from "./Register";
 
 export const AuthPage = () => {
-    return <div>This is the auth page.</div>
+    const [isLogin, setIsLogin] = useState(true)
+
+    // Handler to help if isLogin is T/F
+    const handleAuthPageToggle = () => {
+        setIsLogin((prev) => !prev);
+    }
+
+    return (
+        <div>
+            {isLogin ?
+                (<Login switchAuthHandler={handleAuthPageToggle} />) :
+                (<Register switchAuthHandler={handleAuthPageToggle} />)}
+        </div>
+    );
 };
