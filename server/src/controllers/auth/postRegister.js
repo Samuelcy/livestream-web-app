66343a90ef44b66d431c9010659c13f5a0e65fa5
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-20 10:03:26 -1000

    Create user schema for DB and connect to /register
    User schema for DB connects to the /register path. Registering via Postman then sends the user info to mongoDB.
    
    Update .env file to include "main" so taht "test" doesn't appear in mongoDB

--- server/src/controllers/auth/postRegister.js
+++ server/src/controllers/auth/postRegister.js
@@ -1,3 +1,12 @@
+import User from "../../../models/User.js";
+
 export const postRegister = async (req,res) => {
-    return res.send("Register route");
+    // Sends User to DB
+    const user = await User.create({
+        username: "test",
+        email: 'test@test.com',
+        password: 'test',
+    });
+
+    return res.send("User is added to database");
 }
