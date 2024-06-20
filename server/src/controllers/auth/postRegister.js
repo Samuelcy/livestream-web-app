c09fc300d13de16e36c8d99927b2183437aa0a79
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-20 10:42:33 -1000

    Add JWT and encrypt user detail

--- server/src/controllers/auth/postRegister.js
+++ server/src/controllers/auth/postRegister.js
@@ -1,27 +1,43 @@
 import User from "../../../models/User.js";
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 export const postRegister = async (req, res) => {
     try {
         const { username, email, password } = req.body;
         const userExists = await User.exists({ email })
-        
+
         if (userExists) {
             return res.stats(409).send("Email already in use");
         }
 
         const encryptedPassword = await bcrypt.hash(password, 10);
-    
+
         const user = await User.create({
             username,
             email: email.toLowerCase(),
             password: encryptedPassword,
         })
 
-        // Create JWT token
-        
+        // Create JWT token, encrypt user detail, add secret,
+        const token = jwt.sign({
+            userId: user._id,
+            email,
+        },
+            process.env.TOKEN_KEY,
+            {
+                expiresIn: "240h",
+            }
+        );
 
-        // Send response back to user
+        // Send response back user data and jwt
+        return res.status(201).json({
+            userDetails: {
+                email,
+                username,
+                token,
+            }
+        })
 
     } catch (err) {
         console.log(err)
