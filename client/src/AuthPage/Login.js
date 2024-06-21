3d10e758bac5af00d4b9c5b72e3054f2c109e0db
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-20 19:18:55 -1000

    Connect validation with form and blur functionality
    If the user clicks off, puts an invalid email or password, the login button would be disabled.

--- client/src/AuthPage/Login.js
+++ client/src/AuthPage/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Logo } from "./Logo";
 import { AuthInput } from "./AuthInput";
+import { emailValidationMessage, passwordValidationMessage, validateEmail, validatePassword } from "../shared/validators";
 
 export const Login = ({ switchAuthHandler }) => {
     const [formState, setFormState] = useState({
@@ -27,12 +28,52 @@ export const Login = ({ switchAuthHandler }) => {
         }));
     }
 
+    const handleInputValidatorOnBlur = (value, field) => {
+        let isValid = false;
+        switch (field) {
+            case 'email':
+                isValid = validateEmail(value);
+                break;
+            case 'password':
+                isValid = validatePassword(value);
+                break;
+            default:
+        }
+
+        setFormState(prevState => ({
+            ...prevState,
+            [field]: {
+                ...prevState[field],
+                isValid,
+                showError: !isValid,
+            }
+        }));
+    }
+
     return <div className="login-container">
         <Logo text={'Login to Stream'} />
         <form className="auth-form">
-            <AuthInput field='email' label='Email' value={formState.email.value} onChangeHandler={handleInputValueChange}/>
-            <AuthInput field='password' label='Password' value={formState.password.value} onChangeHandler={handleInputValueChange} />
-            <button> Log In </button>
+            <AuthInput
+                field='email'
+                label='Email'
+                value={formState.email.value}
+                onChangeHandler={handleInputValueChange}
+                type='text'
+                onBlurHandler={handleInputValidatorOnBlur}
+                showErrorMessage={formState.email.showError}
+                validationMessage={emailValidationMessage}
+            />
+            <AuthInput
+                field='password'
+                label='Password'
+                value={formState.password.value}
+                onChangeHandler={handleInputValueChange}
+                type='password'
+                onBlurHandler={handleInputValidatorOnBlur}
+                showErrorMessage={formState.password.showError}
+                validationMessage={passwordValidationMessage}
+            />
+            <button disabled={!formState.password.isValid || !formState.email.isValid}> Log In </button>
         </form>
         <span onClick={switchAuthHandler} className="auth-form-switch-label">
             Don't have an account? Please sign up
