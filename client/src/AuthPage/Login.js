df49a40952b65f644869f846648f5e2ddc455a81
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-20 15:39:44 -1000

    Handle when input value changes

--- client/src/AuthPage/Login.js
+++ client/src/AuthPage/Login.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Logo } from "./Logo";
 import { AuthInput } from "./AuthInput";
 
 export const Login = ({ switchAuthHandler }) => {
+    const [formState, setFormState] = useState({
+        email: {
+            value: '',
+            isValid: false,
+            showError: false,
+        },
+        password: {
+            value: '',
+            isValid: false,
+            showError: false,
+        }
+    })
+
+    // update the state of a specific form field (either "email" or "password") whenever its value change
+    const handleInputValueChange = (value, field) => {
+        setFormState(prevState => ({
+            ...prevState,
+            [field]: {
+                ...prevState[field],
+                value,
+            }
+        }));
+    }
+
+    console.log(formState);
+
     return <div className="login-container">
         <Logo text={'Login to Stream'} />
         <form className="auth-form">
-            <AuthInput field='email' label='Email' />
-            <AuthInput field='password' label='Password' />
+            <AuthInput field='email' label='Email' value={formState.email.value} onChangeHandler={handleInputValueChange}/>
+            <AuthInput field='password' label='Password' value={formState.password.value} onChangeHandler={handleInputValueChange} />
             <button> Log In </button>
         </form>
         <span onClick={switchAuthHandler} className="auth-form-switch-label">
