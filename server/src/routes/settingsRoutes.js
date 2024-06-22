fa57c94fae371215762ba166ec4eff7786809aab
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-22 10:57:02 -1000

    Update channel settings route

--- server/src/routes/settingsRoutes.js
+++ server/src/routes/settingsRoutes.js
@@ -2,12 +2,19 @@ import express from 'express'
 import ExpressValidation from "express-joi-validation"
 import Joi from 'joi'
 import { verifyToken } from "../middlewares/auth.js"
-import { getChannelSettings } from "../controllers/controllers.js"
+import { getChannelSettings, putChannelSettings } from "../controllers/controllers.js"
 
 const router = express.Router();
 
 const validator = ExpressValidation.createValidator({});
 
-router.get('/:channelId', verifyToken, getChannelSettings);
+const channelSettingsSchema = Joi.object({
+    username: Joi.string().min(3).max(12).required(),
+    description: Joi.string().min(10).max(200).required(),
+    title: Joi.string().min(3).max(30).required(),
+    avatarUrl: Joi.string().uri().required(),
+})
 
+router.get('/:channelId', verifyToken, getChannelSettings);
+router.put("/channel", verifyToken, validator.body(channelSettingsSchema), putChannelSettings);
 export default router;
