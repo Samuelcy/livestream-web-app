2782d22870f9556a5f93e889e894d04b6fd36b64
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-21 21:03:06 -1000

    Get channel details
    Grabs channel detail from user, and adds to url

--- server/src/controllers/channels/getChannelDetails.js
+++ server/src/controllers/channels/getChannelDetails.js
@@ -1,10 +1,31 @@
+import User from '../../models/User.js'
+import Channel from '../../models/Channel.js'
+
 export const getChannelDetails = async (req, res) => {
-    return res.status(200).json({
-        id: 1,
-        title: 'Channel',
-        description: 'Description',
-        username: "testUsername",
-        isOnline: false,
-        streamUrl: "http"
-    });
-}
+    try {
+        const { channelId } = req.params;
+        const channel = await Channel.findById(channelId);
+        
+        if (!channel) {
+            return res.status(404).send('Channel not found.');
+        }
+
+        const user = await User.findOne({ channel: channelId }, { username: 1 })
+        const streamUrl = 'http';
+        const isOnline = false;
+
+        return res.status(200).json({
+            id: channel._id,
+            title: channel.title,
+            description: channel.description,
+            username: user.username,
+            isOnline,
+            streamUrl: streamUrl
+        });
+
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send("Something went wrong");
+    }
+};
+
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -5,7 +5,7 @@ const { Schema } = mongoose;
 // User schema in DB
 const userSchema = new Schema({
     email: { type: String, unique: true },
-    usernane: { type: String },
+    username: { type: String },
     password: { type: String },
     channel: { type: Schema.Types.ObjectId, ref: "Channel" },
     followedChannels: { type: [{ type: Schema.Types.ObjectId, ref: "Channel" }] }
