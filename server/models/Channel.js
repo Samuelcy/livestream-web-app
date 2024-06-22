0ca234eab4a34c196b30cfc6b0f239133e385e85
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-21 17:43:12 -1000

    Add user, channel, and message schema

---
+++ server/models/Channel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { v4 as uuid } from "uuid"
+
+const { Schema } = mongoose;
+
+const defaultTitle = "New Channel";
+const defaultDescription = "This is a new channel.";
+
+const channelSchema = new Schema({
+    isActive: { type: Boolean, default: false },
+    title: { type: String, default: defaultTitle },
+    description: { type: String, default: defaultDescription },
+    avatarUrl: { type: String, default: 'none' },
+    streamKey: { type: String, default: uuid() },
+    // Array of messages
+    messages: {
+        type: [{ type: Schema.Types.ObjectId, ref: 'Message' }], 
+        default: [],
+    }
+});
+
+export default mongoose.model("Channel", channelSchema);
--- server/models/User.js
+++ server/models/User.js
@@ -6,7 +6,9 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
     email: { type: String, unique: true },
     usernane: { type: String },
-    password: { type: String }
+    password: { type: String },
+    channel: { type: Schema.Types.ObjectId, ref: "Channel" },
+    followedChannels: { type: [{ type: Schema.Types.ObjectId, ref: "Channel" }] }
 });
 
 export default mongoose.model("User", userSchema);
