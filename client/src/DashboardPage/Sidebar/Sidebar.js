b33abdfb189df65584c732385a5faeb1ea911c04
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-23 10:45:08 -1000

    Render list of following channels

--- client/src/DashboardPage/Sidebar/Sidebar.js
+++ client/src/DashboardPage/Sidebar/Sidebar.js
@@ -1,5 +1,33 @@
 import React from "react";
 
+const followedChannels = [
+    {
+        id: 1,
+        username: 'Martin',
+        isOnline: false,
+    },
+    {
+        id: 2,
+        username: 'Marta',
+        isOnline: true,
+    },
+    {
+        id: 3,
+        username: 'Jack',
+        isOnline: false,
+    },
+
+]
+
 export const Sidebar = () => {
-    return <div className="sidebar-container">Sidebar</div>
+    return <div className="sidebar-container">
+        <span className="sidebar-title">For you</span>
+        <span className="sidebar-subtitle">FOLLOWED CHANNELS</span>
+        {followedChannels.map(channel => {
+            return <div key={channel.id} className='sidebar-list-item'>
+                <span className="sidebar-list-username">{channel.username}</span>
+                <span classname="sidebar-list-status" style={{ color: channel.isOnline ? 'green' : 'red' }}>{channel.isOnline ? "Online" : "Offline"}</span>
+            </div>
+        })}
+    </div>
 }
