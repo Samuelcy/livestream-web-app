62a5a143a7ebbab58d948e95e2883f9328751e57
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-23 12:23:02 -1000

    Add rendered channel

---
+++ client/src/DashboardPage/Content/Channels/ChannelCard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const imageUrl = 'https://media.istockphoto.com/id/1334436084/photo/top-down-view-of-colorful-illuminated-gaming-accessories-laying-on-table.jpg?s=612x612&w=0&k=20&c=E9xnbAZoBS5LlUX0q-zxT_3m6gEZpyB2k51_U4LLMNs='
+
+const ChannelAvatar = ({ url }) => {
+    return (
+        <div className="channels-avatar-container">
+            <img src={url || imageUrl} widht="100%" height="100%" />
+        </div>
+    )
+}
+
+export const ChannelCard = ({
+    title,
+    id,
+    username,
+    isOnline,
+    avatarUrl,
+    navigateToChannelHandler,
+}) => {
+    const handleNavigate = () => {
+        navigateToChannelHandler()
+    }
+
+    return <div className="channels-card" onClick={handleNavigate} >
+        <ChannelAvatar url={avatarUrl} />
+        <span className="channels-card-title">{title}</span>
+        <span className="channels-card-text">{username}</span>
+        <span className="channels-card-text"
+            style={{ color: isOnline ? 'green' : 'red' }}
+        >{isOnline ? "Online" : "Offline"}</span>
+    </div>
+};
--- client/src/DashboardPage/Content/Content.js
+++ client/src/DashboardPage/Content/Content.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { Channels } from "./Channels";
 
 export const Content = () => {
     return <div className="content-container">
         <Routes>
             <Route path='settings' element={<div>Settings</div>} />
-            <Route path='channels' element={<div>Channels</div>} />
+            <Route path='channels' element={<Channels />} />
             {/* Route for a specific channel by ID: /channel/1234 */}
             <Route path='channel/:id' element={<div>Channel</div>} />
         </Routes>
