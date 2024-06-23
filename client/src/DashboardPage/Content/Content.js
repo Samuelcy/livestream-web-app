ecdfc46eb7d58fc3686b6b63de07ca4721d7bd11
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-23 11:50:36 -1000

    Add nested routing
    "Content" container will now render depending on URL

--- client/src/DashboardPage/Content/Content.js
+++ client/src/DashboardPage/Content/Content.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 export const Content = () => {
-    return <div className="content-container">Content</div>
+    return <div className="content-container">
+        <Routes>
+            <Route path='settings' element={<div>Settings</div>} />
+            <Route path='channels' element={<div>Channels</div>} />
+            {/* Route for a specific channel by ID: /channel/1234 */}
+            <Route path='channel/:id' element={<div>Channel</div>} />
+        </Routes>
+    </div>
 }
