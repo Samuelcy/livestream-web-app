847a6ed748157d410ea1fb0e7c9e2dc7b884358e
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-23 16:09:47 -1000

    Add channel view

--- client/src/DashboardPage/Content/ChannelView/ChannelDescription.js
+++ client/src/DashboardPage/Content/ChannelView/ChannelDescription.js
@@ -1,6 +1,14 @@
 import React from "react";
 
-export const ChannelDescription = () => {
-    return <div className="channel-description-container">
-        Channel description</div>
+
+export const ChannelDescription = ({ username, title, description }) => {
+    return (
+        <div className="channel-description-container">
+            <span className="channel-description-title">{username}</span>
+            <span className="channel-description-subtitle">{title}</span>
+            <div className="channel-description-box">
+                <span className="channel-description">{description}</span>
+            </div>
+        </div>
+    );
 };
--- client/src/DashboardPage/Content/ChannelView/ChannelView.js
+++ client/src/DashboardPage/Content/ChannelView/ChannelView.js
@@ -2,14 +2,28 @@ import React from "react";
 import { Chat } from "./Chat";
 import { ChannelDescription } from "./ChannelDescription";
 
+
+const channelDetails = {
+    id: 1,
+    title: 'Gaming Channel',
+    description: "Playing a game",
+    username: "Gamer",
+    isOnline: false,
+}
+
 export const ChannelView = () => {
     return (
         <div className="channel-container">
-            <div className="channel-video-description-container">
+            <div className="channel-video-description-section">
                 <div className="channel-offline-placeholder">
                     <span>Channel is offline</span>
                 </div>
-                <ChannelDescription />
+                <ChannelDescription
+                    channelId={channelDetails.id}
+                    title={channelDetails.title}
+                    description={channelDetails.description}
+                    username={channelDetails.username}
+                />
             </div>
             <Chat />
         </div>
