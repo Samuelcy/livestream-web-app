19158df812b90e08febc6a842f2d7f7e2126ca5e
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-23 19:08:44 -1000

    Rendering inputs through mapping

--- client/src/DashboardPage/Content/Settings/Settings.js
+++ client/src/DashboardPage/Content/Settings/Settings.js
@@ -14,6 +14,7 @@ export const Settings = () => {
     return (
         <div className="settings-container">
             <span>Settings </span >
+            <ChannelSettings settings={channelSettings} />
             <StreamKey streamKey={channelSettings.streamKey} />
         </div>
     );
