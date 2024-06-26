42b922754931d0f6183c239f93c78d3f5933583c
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-25 21:21:19 -1000

    Fetch channels hook

---
+++ client/src/shared/hooks/useChannels.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import { getFollowedChannels, getChannels as getChannelsRequest } from "../../api";
+import toast from "react-hot-toast"
+
+export const useChannels = () => {
+    const [channels, setChannels] = useState(null);
+
+    // If the user is logged, the JWT is attached to the request giving the followed channels
+    const getChannels = async (isLogged = false) => {
+        const channelsData = await getChannelsRequest();
+
+        if (channelsData.error) {
+            return toast.error(
+                channelsData.exception?.response?.data || "Error occured when fetching channel."
+            );
+        }
+
+        // User not logged in
+        if (!isLogged) {
+            return setChannels({
+                channels: channelsData.data.channels,
+            })
+        }
+
+        const followedChannelsData = await getFollowedChannels();
+
+        if (channelsData.error) {
+            return toast.error(
+                channelsData.exception?.response?.data || "Error occured when fetching the followed channels."
+            );
+        }
+
+        setChannels({
+            channels: channelsData.data.channels,
+            followedChannelsData: followedChannelsData.data.followedChannels,
+        })
+    };
+
+    useEffect(() => {
+        console.log(channels);
+    }, [channels]);
+
+    return {
+        getChannels,
+    }
+};
