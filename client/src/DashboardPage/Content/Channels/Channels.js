3d0d0d5c9aaac9c9fda27971e83454c9bbf87114
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-26 14:56:23 -1000

    Clicking on channels route to channel apge

--- client/src/DashboardPage/Content/Channels/Channels.js
+++ client/src/DashboardPage/Content/Channels/Channels.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ChannelCard } from "./ChannelCard";
+import { useNavigate } from 'react-router-dom'
 
 export const dummyChannels = [
     {
@@ -33,17 +34,24 @@ export const dummyChannels = [
 ]
 
 export const Channels = ({ channels }) => {
-    console.log(channels);
+    // console.log(channels);
+
+    const navigate = useNavigate();
+
+    const handleNavigateToChannel = (id) => {
+        navigate(`/channel/${id}`);
+    }
 
     return <div className="channels-container">
         {channels.map(channel => (
             <ChannelCard
                 key={channel.id}
+                id={channel.id}
                 title={channel.title}
                 username={channel.username}
                 isOnline={channel.isOnline}
                 avatarUrl={channel.avatarUrl}
-                navigateToChannelHandler={() => { }}
+                navigateToChannelHandler={handleNavigateToChannel}
             />
         ))}
     </div>
