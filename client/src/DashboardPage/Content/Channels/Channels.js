0358cd5a34a76730cf19d68ac0919d29e4be3273
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-27 18:14:28 -1000

    Clicking on sidebar user leads to user channel

--- client/src/DashboardPage/Content/Channels/Channels.js
+++ client/src/DashboardPage/Content/Channels/Channels.js
@@ -2,36 +2,36 @@ import React from "react";
 import { ChannelCard } from "./ChannelCard";
 import { useNavigate } from 'react-router-dom'
 
-export const dummyChannels = [
-    {
-        id: 1,
-        title: 'test',
-        avatarUrl: null,
-        username: 'Martin',
-        isOnline: false,
-    },
-    {
-        id: 2,
-        title: 'test',
-        avatarUrl: null,
-        username: 'Martin',
-        isOnline: false,
-    },
-    {
-        id: 3,
-        title: 'test',
-        avatarUrl: null,
-        username: 'Martin',
-        isOnline: false,
-    },
-    {
-        id: 4,
-        title: 'test',
-        avatarUrl: null,
-        username: 'Martin',
-        isOnline: false,
-    },
-]
+// export const dummyChannels = [
+//     {
+//         id: 1,
+//         title: 'test',
+//         avatarUrl: null,
+//         username: 'Martin',
+//         isOnline: false,
+//     },
+//     {
+//         id: 2,
+//         title: 'test',
+//         avatarUrl: null,
+//         username: 'Martin',
+//         isOnline: false,
+//     },
+//     {
+//         id: 3,
+//         title: 'test',
+//         avatarUrl: null,
+//         username: 'Martin',
+//         isOnline: false,
+//     },
+//     {
+//         id: 4,
+//         title: 'test',
+//         avatarUrl: null,
+//         username: 'Martin',
+//         isOnline: false,
+//     },
+// ]
 
 export const Channels = ({ channels }) => {
     // console.log(channels);
