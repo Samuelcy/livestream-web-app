dfc5abb2fb5f0656c8b4d60b079bb0812b1ac984
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-29 17:13:25 -1000

    Add new message input

--- client/src/DashboardPage/Content/ChannelView/Chat/NewMessageInput.js
+++ client/src/DashboardPage/Content/ChannelView/Chat/NewMessageInput.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const NewMessageInput = () => {
-    return <div>New Message Input</div>
+export const NewMessageInput = ({ sendMessage }) => {
+    const [messageContent, setMessageContent] = useState("");
+
+    const handleValueChange = (e) => {
+        setMessageContent(e.target.value);
+    }
+
+    const handleSendMessage = () => {
+        // Send message to the server
+
+        // Rests input value after sending message
+        setMessageContent("")
+    }
+
+    const handleKeyPress = (e) => {
+        if (e.key === "Enter") {
+            handleSendMessage();
+        }
+    }
+
+    console.log(messageContent);
+    
+    return <div className="chat-message-input-container">
+        <input
+            className="chat-message-input"
+            placeholder="Type message..."
+            value={messageContent}
+            onChange={handleValueChange}
+            onKeyDown={handleKeyPress}
+        />
+    </div>
 }
