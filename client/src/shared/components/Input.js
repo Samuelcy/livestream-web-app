0af0974773153075d2a41e604ab101a5e7cbe0f0
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-07-21 20:18:40 -1000

    Update input styling

--- client/src/shared/components/Input.js
+++ client/src/shared/components/Input.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { TextField } from "@mui/material";
 
 export const Input = ({
     field,
@@ -14,39 +15,27 @@ export const Input = ({
     // Accepting an event that gives an input , after every single change it will receive a new value
     // passing field to onChangeHandler
     const handleValueChange = (e) => {
-        onChangeHandler(e.target.value, field)
+        onChangeHandler(e.target.value, field);
     };
 
     const handleInputBlur = (e) => {
-        onBlurHandler(e.target.value, field)
+        onBlurHandler(e.target.value, field);
     };
 
     return (
-        <>
-            <div className='auth-form-label'>
-                <span>{label}</span>
-            </div> 
-            {textarea ? (
-                <textarea
-                    type={type}
-                    value={value}
-                    onChange={handleValueChange}
-                    onBlur={handleInputBlur}
-                    rows={5}
-                    style={{ maxWidth: "400px " }}
-                />
-            ) : (
-                <input
-                    type={type}
-                    value={value}
-                    onChange={handleValueChange}
-                    onBlur={handleInputBlur}
-                />
-            )}
-       
-            <span className="auth-form-validation-message">
-                {showErrorMessage && validationMessage}
-            </span>
-        </>
-    )
-}
+        <TextField
+            label={label}
+            value={value}
+            onChange={handleValueChange}
+            onBlur={handleInputBlur}
+            error={showErrorMessage}
+            helperText={showErrorMessage ? validationMessage : ""}
+            type={type}
+            multiline={textarea}
+            rows={textarea ? 5 : 1}
+            fullWidth
+            variant="outlined"
+            margin="normal"
+        />
+    );
+};
